513e12ec1643e6ce37756d8035259c1a52e188c6
カイル <kyle@kylerich.dev>
2026-02-05 12:01:40 -0500

    Create CrunchyrollExternalUrlProvider.cs

---
+++ Jellyfin.Plugin.Crunchyroll/Providers/CrunchyrollExternalUrlProvider.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+using MediaBrowser.Model.Providers;
+
+namespace Jellyfin.Plugin.Crunchyroll.Providers;
+
+/// <summary>
+/// External URL provider for Crunchyroll.
+/// </summary>
+public class CrunchyrollExternalUrlProvider : IExternalUrlProvider
+{
+    /// <inheritdoc/>
+    public string Name => "Crunchyroll";
+
+    /// <inheritdoc/>
+    public IEnumerable<string> GetExternalUrls(BaseItem item)
+    {
+        switch (item)
+        {
+            case Series:
+                if (item.TryGetProviderId(MetadataProvider.Tmdb, out var externalId))
+                {
+                    yield return $"https://www.crunchyroll.com/series/{externalId}";
+                }
+
+                break;
+        }
+    }
+}
